4828087e7ca5bb0923c30a4eda16e675fdf59a59
stijnvdwulp <119616099+stijnvdwulp@users.noreply.github.com>
2022-12-02 12:43:52 +0100

    uploads

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+      }
+    },
+    colors: {
+      'middleblue': '#7DBBC3',
+      'teagreen': '#DAEDBD',
+      'middleyellowred': '#E5B181',
+      'burntsienna': '#DE6B48',
+      'white': '#ffffff',
+      'cultured': '#F1EDEE',
+      'blackcoral': '#3D5467',
+      'morningblue': '#8AA29E',
+      'dimgray': '#686963',
+      'indianred': '#DB5461',
+      'cadet': '#4F6D7A',
+      'columbiablue': '#C0D6DF',
+      'aliceblue': '#DBE9EE',
+      'blueyonder': '#4A6FA5',
+      'bluesapphire': '#16608',
+
+      'charcoal': '#424B54',
+      'pewterblue': '#93A8AC',
+      'copperrose': '#9B6A6C',
+      'rozeextra' : '#E2B4BD',
+      'black':  '#000000',
+
+    }
+  },
+  plugins: [],
+}
